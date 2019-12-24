b9c74b4fa95777551425b35de84f7e4303edd99b
Himzo Tahic <himzotahic@gmail.com>
2019-12-24 20:25:12 +0100

    Create P38_CompareTotients.scala

---
+++ src/test/scala/problems/arithmetic/P38_CompareTotients.scala
@@ -0,0 +1,16 @@
+package problems.arithmetic
+
+import org.scalatest._
+import org.scalatest.prop.TableDrivenPropertyChecks._
+import problems.arithmetic.SolutionsToTheArithmeticProblems._
+
+import scala.util.{Failure, Try}
+
+class P38_CompareTotients extends WordSpec with Matchers {
+  "P38_CompareTotients" should {
+    "return a string describing the performance of the two totient methods" in {
+      val comparison = 10090.compareTotients
+      println(comparison)
+    }
+  }
+}
